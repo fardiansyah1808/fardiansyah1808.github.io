4c8399291b1da34c9aaae1508c6837efe5c8df02
Fardi Ansyah <fardiansyah@ifca.co.id>
2024-06-07 11:43:31 +0700

    perubahan

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import PlaceContentCenter from './components/PlaceContentCenter.jsx';
+import Todo from './components/Todo.jsx';
+
+export default function App() {
+    return (
+        <PlaceContentCenter>
+            <Todo />
+        </PlaceContentCenter>
+    );
+}

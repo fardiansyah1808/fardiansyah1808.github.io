ffa73fc6105b2744cbf039212168f0504785158c
Fardi Ansyah <fardiansyah@ifca.co.id>
2024-06-07 11:50:09 +0700

    publish

--- src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-export default function Input({ type = 'text', ...props }) {
-    return (
-        <input
-            type={type}
-            {...props}
-            className='transition border-slate-300 shadow-sm rounded-lg w-full focus:outline-none focus:ring focus:border-violet-300 focus:ring-violet-300'
-        />
-    );
-}
--- src/components/Label.jsx
+++
@@ -1,7 +0,0 @@
-export default function Label({ value, children, ...props }) {
-    return (
-        <label className='text-slate mb-1 block' {...props}>
-            {value || children}
-        </label>
-    );
-}

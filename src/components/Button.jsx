ad495209cbd7a3dc47bf5f9cb36df1fa57ff8a14
Fardi Ansyah <fardiansyah@ifca.co.id>
2024-06-07 17:02:14 +0700

    fix: todos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button(props) {
     const {
         children,
         text,
-        className = 'bg-neutral-900',
+        className = 'bg-neutral-900 text-violet-50',
         type = 'submit',
     } = props;
     return (
@@ -13,7 +13,7 @@ export default function Button(props) {
             type={type}
             className={clsx(
                 className,
-                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-2 inline-flex justify-center whitespace-nowrap gap-x-1 items-center text-violet-50 px-4 py-2 rounded-md',
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-2 inline-flex justify-center whitespace-nowrap gap-x-1 items-center px-4 py-2 rounded-md',
             )}>
             {text || children}
         </button>
